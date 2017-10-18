bfe2fcac05fb8109d29c5fc7472d58cae9f44504
guoliangliang <liangliang.guo@nbbsw.com>
2017-10-18 16:32:48 +0800

    wrote a test code

---
+++ hello.java
@@ -0,0 +1,12 @@
+
+
+public class hello {
+    public static void main(String[] args) {
+		String str1 = "abcd";
+		String str2 = "abcc";
+
+		System.out.println(str1.compareTo(str2));
+        }
+      
+    
+}
